2abab816011e06552a788d00e271227ab0ff941b
Abhishek Kandikattu <abhishekkandikattu@Abhisheks-MacBook-Air.local>
2024-11-23 16:20:03 -0800

    Refactored Service code

--- src/main/java/com/fetchproject/fetch_project/service/ReceiptService.java
+++ src/main/java/com/fetchproject/fetch_project/service/ReceiptService.java
@@ -4,7 +4,6 @@ import com.fetchproject.fetch_project.model.Item;
 import com.fetchproject.fetch_project.model.Receipt;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -25,28 +24,32 @@ public class ReceiptService {
         return receipt;
     }
 
-    private int calculatePointsForPurchaseTime(Receipt receipt) {
-        String purchaseTime = receipt.getPurchaseTime();
-        if(purchaseTime != null) {
-            List<String> purchaseTimeList = List.of(purchaseTime.split(":"));
-            int purchaseHour = Integer.parseInt(purchaseTimeList.getFirst());
-            if(purchaseHour >= 14 && purchaseHour <= 16) {
-                return 10;
-            }
-        }
+    private int calculatePointsForRetailer(Receipt receipt) {
+        String retailerName = receipt.getRetailer();
+        if(retailerName != null)
+            return retailerName.replaceAll("[^a-zA-Z0-9]", "").length();
         return 0;
     }
 
-    private int calculatePointsForPurchaseDate(Receipt receipt) {
-        String date = receipt.getPurchaseDate();
-        if(date != null){
-            List<String> dateList = List.of(date.split("-"));
-            /* Ideally should log as an error in date format.
-            For the purpose of this assignment returning as zero. */
-            if(dateList.size() < 3)
-                return 0;
-            int day = Integer.parseInt(dateList.get(2));
-            if(day % 2 != 0) return 6;
+    private int calculatePointsForTotalAmount(Receipt receipt) {
+        double total = receipt.getTotal();
+        int points = 0;
+        if(total > 0.0){
+            if(total == Math.floor(total)) {
+                points += 50;
+            }
+            if(total % 0.25 == 0) {
+                points += 25;
+            }
+        }
+        return points;
+    }
+
+    private int calculatePointsForNumberOfItems(Receipt receipt) {
+        List<Item> itemsList = receipt.getItems();
+        if(itemsList != null) {
+            int length = (int) Math.floor((double) itemsList.size() / 2);
+            return length * 5;
         }
         return 0;
     }
@@ -66,33 +69,29 @@ public class ReceiptService {
         return points;
     }
 
-    private int calculatePointsForNumberOfItems(Receipt receipt) {
-        List<Item> itemsList = receipt.getItems();
-        if(itemsList != null) {
-            int length = (int) Math.floor((double) itemsList.size() / 2);
-            return length * 5;
+    private int calculatePointsForPurchaseDate(Receipt receipt) {
+        String date = receipt.getPurchaseDate();
+        if(date != null){
+            List<String> dateList = List.of(date.split("-"));
+            /* Ideally should log as an error in date format.
+            For the purpose of this assignment returning as zero. */
+            if(dateList.size() < 3)
+                return 0;
+            int day = Integer.parseInt(dateList.get(2));
+            if(day % 2 != 0) return 6;
         }
         return 0;
     }
 
-    private int calculatePointsForTotalAmount(Receipt receipt) {
-        double total = receipt.getTotal();
-        int points = 0;
-        if(total > 0.0){
-            if(total == Math.floor(total)) {
-                points += 50;
-            }
-            if(total % 0.25 == 0) {
-                points += 25;
+    private int calculatePointsForPurchaseTime(Receipt receipt) {
+        String purchaseTime = receipt.getPurchaseTime();
+        if(purchaseTime != null) {
+            List<String> purchaseTimeList = List.of(purchaseTime.split(":"));
+            int purchaseHour = Integer.parseInt(purchaseTimeList.getFirst());
+            if(purchaseHour >= 14 && purchaseHour <= 16) {
+                return 10;
             }
         }
-        return points;
-    }
-
-    private int calculatePointsForRetailer(Receipt receipt) {
-        String retailerName = receipt.getRetailer();
-        if(retailerName != null)
-            return retailerName.replaceAll("[^a-zA-Z0-9]", "").length();
         return 0;
     }
 }
